81c99b18d8248ada038441c912204a29d0d26dfe
Erin Walker <erinb0317@hotmail.com>
2018-02-18 21:07:56 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,13 +3,13 @@ module Players
     #attr_accessor :game
 
     def move(board)
-      move = nil 
+      move = nil
 
       if !board.taken?(5)
         move = "5"
       elsif board.turn_count >= 2
-        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s 
-      else 
+        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+      else
         Game::WIN_COMBINATIONS.detect do |combo|
           if combo.select{|i| board.position(i+1) == token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
             move = combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
@@ -17,6 +17,7 @@ module Players
             move = combo.select{|i| !board.taken?(i+1)}.first.to_i+(1).to_s
           end
         end
+      end 
 
       #def almost_won
       #  @game.win_indexes.index{|index| index == ["#{self.token}","#{self.token}", " "] ||
